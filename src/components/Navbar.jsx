84511e90db897e71538b5603907ec3eca702739e
shubham <shuubhamsharma98@gmail.com>
2024-06-27 23:05:32 -0500

    sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from '@mui/material'
 import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
-
+import SearchBar from './SearchBar';
 const Navbar = () => 
    (
     <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
@@ -9,6 +9,7 @@ const Navbar = () =>
 <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
     </Stack>
     
   )
